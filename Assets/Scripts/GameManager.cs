d7be1d656b6d665d5221eab1ed4f7fa700088bbf
AnthonyBarbaro <anthonybarbaro40@gmail.com>
2024-10-31 17:36:32 -0700

    Updates BlackJack - Hitting cards, etc

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,19 @@ public class GameManager : MonoBehaviour
     public Button standBtn;
     public Button betBtn;
 
+    private int standClicks = 0;
+
+    // all the Text elements 
+    public Text standBtnText;
+    public Text scoreText;
+    public Text dealerScoreText;
+    public Text betsText;
+    public Text cashText;
+    //public Text mainText;
+    // Card Hiding Dealers 2nd Card
+    public GameObject hideCard;
+    int pot = 0;
+
     // Access the player and dealers script
 
     public PlayerScript playerScript;
@@ -24,18 +37,48 @@ public class GameManager : MonoBehaviour
     } 
     private void DealClicked()
     {
+        //Hide dealer hand score at the start
+        dealerScoreText.gameObject.SetActive(false);    
         GameObject.Find("DECK").GetComponent<DeckScript>().Shuffle();
         playerScript.StartHand();
         dealerScript.StartHand();
+        // to display Score 
+        scoreText.text = "Hand: " +  playerScript.handValue.ToString();
+        dealerScoreText.text = "Dealer Hand: " + dealerScript.handValue.ToString();
+
+        //remove the buttons after deal 
+
+        dealBtn.gameObject.SetActive(false);
+        hitBtn.gameObject.SetActive(true);
+        standBtn.gameObject.SetActive(true);
+        standBtnText.text = "Stand";
+        // Set Standard Pot Size
+        pot = 40;
+        betsText.text = pot.ToString();
+
     }
     private void HitClicked()
     {
-        throw new NotImplementedException();
+        //Check that there is room
+        if(playerScript.GetCard() <= 10)
+        {
+            playerScript.GetCard();
+        }
     }
  
     private void StandClicked()
     {
-        throw new NotImplementedException();
+        standClicks++;
+        if (standClicks > 1) Debug.Log("end Function");
+        HitDealer();
+        standBtnText.text = "Call";
+    }
+    private void HitDealer()
+    {
+        while (dealerScript.handValue < 16 && dealerScript.cardIndex < 10)
+        {
+            dealerScript.GetCard();
+        }
     }
 
 
